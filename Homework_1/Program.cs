c9464bf7afc28f69cde9090a28d1c59e7d9b85b9
Aleksandra Obukh <sem0609@mail.ru>
2022-12-07 21:44:10 +0300

    Solved task 1

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+int numReversed = 0;
+
+if(num < 0 || (num % 10 == 0 && num != 0))
+{
+    Console.WriteLine("No!");
+    return;
+}
+while (num > numReversed)
+{
+    numReversed = numReversed * 10 + num % 10;
+    num /= 10;
+}
+if (num == numReversed || num == numReversed/10)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
